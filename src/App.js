624e8af78543ec57060179d93a04354ee67294e4
kazi maruf <kazimaruf008@example.com>
2021-03-11 03:32:39 +0400

    p

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js'
 import './App.css';
 import Home from './components/Home/Home';
 import TeamDetails from './components/TeamDetails/TeamDetails';
