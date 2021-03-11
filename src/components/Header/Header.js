5c2f9162111195b411d59b4e0adf3a0297c653bf
kazi maruf <kazimaruf008@example.com>
2021-03-11 06:22:55 +0400

    submite

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,29 +6,29 @@ const Header = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-                <a className="navbar-brand logo" href="#">The FB Camp</a>
+                <a className="navbar-brand logo" href="/">The FB Camp</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
                     <ul className="navbar-nav">
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">Blog</a>
+                        <a className="nav-link" href="/">Blog</a>
                     </li>
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">About</a>
+                        <a className="nav-link" href="/">About</a>
                     </li>
                     <li className="nav-item dropdown active">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <a className="nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Times
                         </a>
                     <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a className="dropdown-item" href="#">Action</a>
-                        <a className="dropdown-item" href="#">Another action</a>
-                        <a className="dropdown-item" href="#">Something else here</a>
+                        <a className="dropdown-item" href="/">Action</a>
+                        <a className="dropdown-item" href="/">Another action</a>
+                        <a className="dropdown-item" href="/">Something else here</a>
                     </div>
                     </li>
                     </ul>
