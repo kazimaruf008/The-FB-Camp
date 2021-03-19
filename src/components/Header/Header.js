a71ddb686ae01ccf29a194f228b17ee46ff22bab
kazi maruf <kazimaruf008@example.com>
2021-03-19 10:06:06 +0400

    oo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,14 +6,14 @@ const Header = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-                <a className="navbar-brand logo" href="/">The FB Camp</a>
+                <a className="navbar-brand logo" href="../Home/Home.js">The FB Camp</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
                     <ul className="navbar-nav">
                     <li className="nav-item active">
-                        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                        <a className="nav-link" href="../Home/Home.js">Home <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item active">
                         <a className="nav-link" href="/">Blog</a>
